4c58b5e2c268f16279d685705214c05a429195f0
mert dedeoglu <mert.dedeoglu.1903@hotmail.com>
2024-07-02 00:31:20 +0300

    Add project files.

---
+++ ChainOfResponbility/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChainOfResponbility.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
